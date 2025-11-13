ce8d447ec1fc1957a4293438b220fc3c4d3ad560
Eduardo Bachtold <eduardobachtold@gmail.com>
2025-11-13 01:18:10 -0300

    atualizar app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 
 # Conexão com Supabase
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não definida nas variáveis de ambiente!")
+
 engine = create_engine(DATABASE_URL)
 
 
